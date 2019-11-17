6b5203ae36c1d453ba4a21094d7c78b46b9cc5c9
Kevin Arias <kevinlh1337@gmail.com>
2019-11-17 12:53:16 -0500

    Se sube el proyecto a github.

---
+++ enlaceimagen.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    Rogue Wow
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <h1> 
+        Rogue World of Warcraft
+    </h1>
+            <p>
+                    Los picaros son una clase que hace daño cuerpo a cuerpo, que visten cuero y que pueden hacer grandes cantidades de daño
+                    a sus enemigos con ataques  muy rápidos. Son maestros del sígilo y el asesinato, pasan entre los enemigos sin ser vistos, 
+                    los atacan desde las sombras y luego escapan en un parpadeo.
+             </p>
+             <p>
+                    Los Pícaros tienen un poderoso arsenal de habilidades de cuerpo a cuerpo, muchas de ellas fortalecidas por su habilidad de sigilo e incapacitar, les cuesta muy poco mantener a un enemigo quieto y aturdido en el lugar por mucho tiempo con habilidades como  Golpe bajo. Son capaces de utilizar venenos para hacer daño extra o para brindar utilidades adicionales a su grupo.
+             </p>
+             <p>
+                    Los Pícaros pueden utilizar una gran variedad de armas de cuerpo a cuerpo tales como dagas, armas de puño, mazos de una mano, espadas de una mano y hachas de una mano. Las diferentes ramas de especialización sacan mas o menos provecho de los diferentes tipos de armamentos. Por ejemplo un Pícaro Asesinato usará dagas para poder beneficiarse de la habilidad  Mutilar.
+             </p>
+    <img id="imagen2" src="imagenes/picaro.jpg">
+    <br>
+<a href="index.php">Volver</a> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    Primer página web.
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <!-- Comentario -->
+    <h1>
+        Esta página es una prueba de programación con HTML, PHP. 
+    </h1>
+    <h2>
+        Subtitulo
+    </h2>
+    <h3>
+        Contenido de información
+    </h3>
+    <h4>
+        Texto pequeño
+    </h4>
+    <h5>
+        Texto más pequeño
+    </h5>
+    <h6>
+        Muy pequeño
+    </h6>
+    <p>
+        Esto es un parrafo para poner mucha información
+    </p>
+    <p>
+        Texto con estilo <b>negrita</b>
+    </p>
+    <a href="http://www.google.com/"target="_blank">Click aquí para ir a google</a>
+    <br>
+    Prueba br
+    <br>
+    Prueba br 2
+    <br>
+    <a href="enlaceimagen.php"><img id="imagen1" src="imagenes/rogue.jpg"></a>
+</body>
+</html>
